4c88a385fcee023eb02c646e09bcffe96e49c2b6
rosines-almeida <rosines.almeida@gmail.com>
2018-11-02 19:34:19 -0200

    add logo e add funçao remove

--- public/dist/app.js
+++ public/dist/app.js
@@ -5,6 +5,20 @@ var USER_ID = getUserId();
 
 
 $(document).ready(function() {
+
+  function autoCompleteType() {
+    var autoCompleteType = [];
+    for (type in  listMedicaments) {       
+    var name = listMedicaments[type].name
+    autoCompleteType.push(name)
+  }
+    $(function() {
+      $("#input-task").autocomplete({
+        source: autoCompleteType
+      });
+  });
+}
+autoCompleteType();
   getUserTasksFromDB();
   $(".send-task").click(addTasks);
 });
@@ -62,26 +76,69 @@ var med2 = '';
 
 
 function createTaskItem(text, key) {
-    $(".task-list").append(`<div class="col-sm-3">
+    $(".task-list").append(`<div class="col-lg-2 col-md-3 col-sm-3 mt-3 ">
     <div class="card ">
          
     <div class="card"  card-color>
       <div class="card-body">
-       
-        <h6 class=" d-flex justify-content-center card-subtitle mb-2 text-muted">${text} </h6>
+      <div class='d-flex justify-content-center mb-4' > <i class="fas fa-pills"></i> </div>
+        <h6 class=" d-flex justify-content-center card-subtitle mb-2"> <b> ${text}</b> </h6>
         <hr>
-        <input type="checkbox"  class="new" data-id=${key} /> <span> Verificar interção </span> <br>
-        <a href="#" class="card-link"><i class="fas fa-info-circle"></i></a> Mais informação 
+        <input type="checkbox"  class="new" data-id=${key} /> <span class ='card-font'> Verificar interação </span> <br>
+        <a class ='info' href="#" class="card-link" data-id=${text} data-toggle='modal' data-target='#modalInfo'> <i class="fas fa-info-circle"></i></a> <span   class ='card-font'>Mais informação </span><br>
+        <a class='removerItens' href="#" class="card-link" data-id=${key}><i class="fas fa-trash-alt"></i></a><span> Remover </span>
       </div>
     </div>
     </div>
 </div>`
 );
+// remover
+$(`.removerItens[data-id='${key}']`).click(function() {
+  console.log(key)
+  var listItem = $(this).parent();
+  console.log(listItem)
+  deleteTask(listItem, key);
+});
  
+
+function deleteTask(listItem, key) {
+deleteTaskFromDB(key);
+listItem.remove();
+}
+
+function deleteTaskFromDB(key) {
+database.ref(`tasks/${USER_ID}/${key}`).remove();
+}
+ 
+
+// ver mais informação
+$(`.info[data-id='${text}']`).click(function(){
+ 
+  $('#title-info').html('');
+  $('#classe-info').html('');
+  $('#posologia-info').html('');
+  $('#alimentar-info').html('');
+  // console.log(text)
+    for (i in listMedicaments){ 
+     
+      if (text == listMedicaments[i].name) {
+        var nameInfo = listMedicaments[i].name.toUpperCase()
+          
+        $('#title-info').append('<strong>'+nameInfo + '</strong>') 
+        $('#classe-info').append(listMedicaments[i].classe)
+        $('#posologia-info').append(listMedicaments[i].posologia)
+        $('#alimentar-info').append(listMedicaments[i].interAlimentos)
+      } 
+    }
+  })
+// remover item
+ 
+ 
+
 $(`input[data-id='${key}']`).click(function() {
 $('#input-section-two').css('display','none') 
 
-   
+
 // var texto = text; 
  
 $('#texto').append('<input class=" comparar mt-5 ml-5 "type="text" value =' + text + '>')
@@ -98,14 +155,17 @@ for ( i in medHtml ){
  
  }
   }//fim element 
-  
+  //more infor
+ 
+
+
 //função para verificar interação
  $('.btn-inter').click(function(){
   med1= medOneTwo[0];
   med2 = medOneTwo[5];
-  var med3 = document.getElementById('med1').value
+  var med3 = document.getElementById('med1').value.toLowerCase();
  
-  var med4 = document.getElementById('med2').value
+  var med4 = document.getElementById('med2').value.toLowerCase();
  
 //pegando o  medicamento 1 ou 3
  listMedicaments.map(function(med){
@@ -122,10 +182,15 @@ for ( i in medHtml ){
  var interaction = inter.map(function (inter) {
    for (i in inter){
      if (med2 == inter[i].nome || med4==inter[i].nome) {
-   $('#descricao').append('<div class= col s12 m7><div class="card horizontal card-panel teal"><div class="card-image"> </div <div class="card-stacked"><div class="card-content"><p>' +inter[i].descricao + '</p> </div> </div></div></div>')
+      $('#descricao').html('');
+      $('#med1').val('')
+      $('#med2').val('')
+      $('.comparar').val('')
+      
+   $('#descricao').append(inter[i].descricao)
    } 
      else if ( med2 != inter[i].nome || med4 != inter[i].nome){
-       $('#descricao').append('<div class= col s12 m7><div class="card horizontal card-panel teal"><div class="card-image"> </div <div class="card-stacked"><div class="card-content"><p> Não foram encontradas interações entre os medicamentos consultados. </p> </div> </div></div></div>');
+       $('#descricao').append( 'Não foram encontradas interações entre os medicamentos consultados.');
        console.log('Não há interação.')
       }
 
@@ -134,7 +199,8 @@ for ( i in medHtml ){
  }); //fim interaction
 }) //fim do click
   
-  
+// $('#descricao').append('<div class= col s12 m7><div class="card horizontal card-panel teal"><div class="card-image"> </div <div class="card-stacked"><div class="card-content"><p>' +inter[i].descricao + '</p> </div> </div></div></div>')
+// } 
 
 
 // function deleteTask(listItem, key) {
--- public/dist/data.js
+++ public/dist/data.js
@@ -1,46 +1,113 @@
 listMedicaments = [
     {
-    name: 'aas',
-    interacao: [
-        {
-        nome: 'captopril',
-        descricao: 'O ácido acetilsalicílico pode diminuir a ação anti-hipertensiva do captopri'
+        name: 'aas',
+        classe:'Anti-inflamatório, analgésico e antitérmico.',
+            posologia:'Deve ser ingerido de preferência após as refeições ou com um pouco de leite. Evite tomar este medicamento em jejun',
+       
+             interacao: [
+            {
+                nome: 'captopril',
+                  descricao: 'O ácido acetilsalicílico pode diminuir a ação anti-hipertensiva do captopril.',
     },
-    {
-    nome: 'insulina',
-    descricao: 'O AAS pode aumentar a ação hipoglicemiante da insulina'
+             {
+                nome: 'insulina',
+                 descricao: 'O AAS pode aumentar a ação hipoglicemiante da insulina'
     }
-    ]
-     }, //fim aas
-    {
-    name: 'omeprazol',
-    interacao: [
-       {
-        nome: 'fenobarbital',
-        descricao: 'O omeprazol usado com fenobarbital (anticonvulsivante) pode potencializar a ação do barbitúrico'
-    },
+    ],
+            interAlimentos: 'Não foram observadas interações na administração concomitante com alimentos.'    
+
+            
+     },
      {
-          nome: "varfarina",
-          descricao: 'O omeprazol  pode aumentar a ação da varfarina  e diminuir a ação do clopidogrel (antitrombóticos)'
-        }
-     ] 
-     },//fim omeprazol
-     
-         {
-        name:'a',
-        interacao: [
-           {
-            nome: 'b',
-            descricao: 'Pode aumentar a probabilidade de reações alérgicas de pele.'
-        } ]
-         }, //fim omeprazol //fim omeprazol//fim omeprazol //fim omeprazol
+        name: 'captopril',
+            classe:'Anti-hipertensivos',
+            posologia:'Tomar captopril 1 hora antes das refeições por via oral.',
+            
+             interacao: [
+            {
+                nome: 'aas',
+                  descricao: 'O ácido acetilsalicílico pode diminuir a ação anti-hipertensiva do captopril.',
+    },
+              ],
+              interAlimentos: 'Alimentos diminuem absorção em 30-40%, evite o uso concomitante com alimentos.' 
+     }, //fim captopril 
+    {
+        name: 'omeprazol',
+        classe:'Antiúlcera',
+            posologia:'Tomar 01 cápsula, em jejum, 30 minutos antes da comida (de preferência, café da manhã), com um copo grande cheio de água.',
+          
+         interacao: [
+            {
+                nome: 'fenobarbital',
+                 descricao: 'O omeprazol usado com fenobarbital (anticonvulsivante) pode potencializar a ação do barbitúrico'
+    },
+             {
+                nome: "varfarina",
+                 descricao: 'O omeprazol  pode aumentar a ação da varfarina  e diminuir a ação do clopidogrel (antitrombóticos).Se necessário o uso concomitante, devem ser considerados os efeitos tóxicos)'
+        },
     
-         {
-            name:'heparina',
-            interacao: [
-               {
+     ],
+     interAlimentos: 'Não foram observadas interações na administração concomitante de omeprazol com alimentos.' 
+     }, //fim omeprazol
+     {
+        name: 'fenobarbital',
+        classe:'Anticonvulsivante',
+            posologia:'2 a 3mg/kg/dia em dose única ou fracionada.',
+           
+         interacao: [
+            {
+                nome: 'omeprazol',
+                 descricao: 'O omeprazol usado com fenobarbital (anticonvulsivante) pode potencializar a ação do barbitúrico'
+    },
+    ],
+    interAlimentos: 'Deve-se evitar a ingestão de qualquer quantidade de álcool.' 
+     },//fim do fenobarbital
+     {
+        name: 'varfarina',
+        classe:'Anticoagulantes',
+        posologia:'Pode ser administrada com ou sem alimentos. Tomar sempre no mesmo horário do dia.',
+         interacao: [
+            {
+                nome: 'omeprazol',
+                 descricao: 'O omeprazol  pode aumentar a ação da varfarina  e diminuir a ação do clopidogrel (antitrombóticos).'
+    },
+    ],
+    interAlimentos: 'Alimentos contendo vitamina K alteram a eficácia deste medicamento.' 
+
+     },
+    {
+        name: 'a',
+        classe:' a ',
+        posologia:'a ',
+         interacao: [
+            {
+                nome: 'b',
+                 descricao: 'Pode aumentar a probabilidade de reações alérgicas de pele.'
+        }],
+        interAlimentos: 'Alimentos  a.' 
+         },  
+
+    {
+        name: 'heparina',
+        classe:'Anticoagulantes ',
+        posologia:' ',
+         interacao: [
+            {
                 nome: 'cetoprofeno',
-                descricao: 'O risco de reações adversas e hemorragias pode ser aumentado. Se a co-administração for necessária, realizar cuidadosa monitorização clínica e laboratorial.'
-            } ]
-             } 
+                 descricao: 'O risco de reações adversas e hemorragias pode ser aumentado. Se a co-administração for necessária, realizar cuidadosa monitorização clínica e laboratorial.'
+            }],
+            interAlimentos: ' .' 
+             },
+             {
+                name: 'cetoprofeno',
+                classe:'Anti-inflamatório, analgésico e antitérmico.',
+            posologia:'Os comprimidos devem ser tomados sem mastigar, com quantidade suficiente de líquido (aproximadamente ½ a 1 copo), de preferência durante ou após as refeições. ',
+            risco:' .',
+                 interacao: [
+                    {
+                        nome: 'heparina',
+                         descricao: 'O risco de reações adversas e hemorragias pode ser aumentado. Se a co-administração for necessária, realizar cuidadosa monitorização clínica e laboratorial.'
+                    }],
+                    interAlimentos: 'Não foram observadas interações na administração concomitante de omeprazol com alimentos.' 
+                     }
          ] //fim list
